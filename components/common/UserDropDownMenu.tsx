6009a9f1b89d438e101fa2aaf7797a08211084e7
SeniorIntern <tnikhil.official@gmail.com>
2024-07-09 22:27:18 +0545

    Fix background color on focus

--- components/common/UserDropDownMenu.tsx
+++ components/common/UserDropDownMenu.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,24 +10,47 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
 import getCookieUser from '@/helpers/getCookieUser';
-import Link from 'next/link';
 import { isNull } from 'lodash';
+import Link from 'next/link';
 
 const UserDropDown = () => {
   const user = getCookieUser();
 
+  const navOptions = [
+    {
+      href: '/profile',
+      label: 'Profile'
+    },
+    {
+      href: '/orders',
+      label: 'Orders'
+    },
+    {
+      href: '/wishlist',
+      label: 'Wishlist'
+    }
+  ];
+
+  if (isNull(user)) return <Link href="/login">Login</Link>;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        {isNull(user) ? <Link href="/login">Login</Link> : user.username}
+        <Avatar className="size-8">
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Profile</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+        {navOptions.map((item, idx) => (
+          <DropdownMenuItem key={idx} className="focus:bg-accent/40">
+            <Link href={item.href} className="size-full focus:text-white">
+              {item.label}
+            </Link>
+          </DropdownMenuItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   );

9ee6fead48c51e094d7a613d7534944cd7b5fb82
SeniorIntern <tnikhil.official@gmail.com>
2024-09-09 10:27:26 +0545

    Add contact page

---
+++ app/(public)/contact/ContactForm.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  fullname: z.string().min(2).max(50),
+  email: z.string().email(),
+  phoneNumber: z.string().min(8).max(16),
+  message: z
+    .string()
+    .min(10, { message: 'Too short message' })
+    .max(200, { message: 'Message is too long' })
+});
+
+const ContactForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullname: '',
+      email: '',
+      phoneNumber: '+971 ',
+      message: ''
+    }
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="fullname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Full Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Enter your email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="phoneNumber"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone Number</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Any message?</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Love to hear from you" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button className="w-full" type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default ContactForm;
--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import { HorizontalPaddingContainer } from '@/components/reusables/HorizontalPaddingContainer';
+import ContactImage from '@/public/contact/contact.png';
+import Image from 'next/image';
+import ContactForm from './ContactForm';
 
 export default function Page() {
-  return <div>Page</div>;
+  return (
+    <section className="my-6 space-y-8">
+      <div className="mx-auto max-w-[400px] space-y-2">
+        <p className="text-center">Contact Us</p>
+        <p className="text-center text-4xl font-bold">
+          We would love to hear from you
+        </p>
+      </div>
+
+      <HorizontalPaddingContainer>
+        <div className="grid grid-cols-1 gap-16 md:grid-cols-2">
+          <div className="flex justify-center md:justify-end">
+            <div className="relative size-[400px]">
+              <Image
+                src={ContactImage}
+                alt="Person communicating with assistant"
+                layout="fill"
+                objectFit="contain"
+                className=""
+              />
+            </div>
+          </div>
+
+          <div>
+            <p className="mx-auto max-w-fit border-b-4 border-gray-300 text-xl font-extrabold">
+              Get In Touch!
+            </p>
+            <ContactForm />
+          </div>
+        </div>
+      </HorizontalPaddingContainer>
+    </section>
+  );
 }

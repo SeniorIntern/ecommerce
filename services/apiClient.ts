4188c65f037f323f44b594276a7b52cdbe19a98c
SeniorIntern <tnikhil.official@gmail.com>
2024-09-19 12:22:28 +0400

    Fix base url of apiclient

--- services/apiClient.ts
+++ services/apiClient.ts
@@ -5,7 +5,7 @@ import { getCookie } from 'cookies-next';
 const token = getCookie(COOKIES.ACCESS_TOKEN);
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1`,
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${token}`
